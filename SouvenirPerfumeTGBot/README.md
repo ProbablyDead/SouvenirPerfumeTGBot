061d036cf651bb52932213a360582721af5fddd3
ProbablyDead <97610135+ProbablyDead@users.noreply.github.com>
2024-02-28 20:05:33 +0300

    clean up

--- SouvenirPerfumeTGBot/README.md
+++
@@ -1,1 +0,0 @@
-### Souvenir perfume tg bot
--- bot/main.py
+++ bot/main.py
@@ -16,15 +16,17 @@ logging.basicConfig(level=logging.INFO)
 
 dist = get_dist()
 
-bot = Bot(token = BOT_TOKEN, parse_mode = ParseMode.HTML)
+bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
 
 dp = Dispatcher()
 router = Router()
 dp.include_router(router)
 dp.include_router(pr)
 
+
 async def main():
     await dp.start_polling(bot)
+
 
 @router.message(Command("start"))
 async def cmd_start(message: types.Message):
@@ -41,14 +43,16 @@ async def cmd_start(message: types.Message):
     )
     await message.answer("Приветствуем вас в нашем замечательном боте!", reply_markup=keyboard)
 
+
 @router.message(F.text.lower() == "пройти тест")
 async def start_poll(message: types.Message):
     id = message.from_user.id
-    dist[f"{id}" + "_0"] = "joined"                                 #не терять
+    dist[f"{id}" + "_0"] = "joined"  # не терять
     await message.answer(f"{dist[f"{id}" + "_0"]}")
     await start_polls(message)
-    
-@router.message(F.text.lower() == "заказать аромат")                #Переделать
+
+
+@router.message(F.text.lower() == "заказать аромат")  # Переделать
 async def get_list(message: types.Message):
     id = message.from_user.id
     for key in dist:
